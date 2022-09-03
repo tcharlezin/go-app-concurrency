6f9bbfd8d00eec06be05ddc181c78f3cb708d51e
Tcharles Pereira da Silva <tcharlezin@gmail.com>
2022-09-03 09:31:41 -0300

    Add interfaces for models and setup test for use mock db

---
+++ data/test-models.go
@@ -0,0 +1,138 @@
+package data
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+)
+
+type UserTest struct {
+	ID        int
+	Email     string
+	FirstName string
+	LastName  string
+	Password  string
+	Active    int
+	IsAdmin   int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Plan      *Plan
+}
+
+type PlanTest struct {
+	ID                  int
+	PlanName            string
+	PlanAmount          int
+	PlanAmountFormatted string
+	CreatedAt           time.Time
+	UpdatedAt           time.Time
+}
+
+func TestNew(dbPool *sql.DB) Models {
+	db = dbPool
+
+	return Models{
+		User: &UserTest{},
+		Plan: &PlanTest{},
+	}
+}
+
+func (u *UserTest) GetAll() ([]*User, error) {
+	var users []*User
+
+	user := User{
+		ID:        1,
+		Email:     "admin@example.com",
+		FirstName: "Admin",
+		LastName:  "Admin",
+		Password:  "abc",
+		Active:    1,
+		IsAdmin:   1,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	users = append(users, &user)
+	return users, nil
+}
+
+func (u *UserTest) GetByEmail(email string) (*User, error) {
+	user := User{
+		ID:        1,
+		Email:     "admin@example.com",
+		FirstName: "Admin",
+		LastName:  "Admin",
+		Password:  "abc",
+		Active:    1,
+		IsAdmin:   1,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	return &user, nil
+}
+
+func (u *UserTest) GetOne(id int) (*User, error) {
+	return u.GetByEmail("")
+}
+
+func (u *UserTest) Update() error {
+	return nil
+}
+
+func (u *UserTest) Delete() error {
+	return nil
+}
+
+func (u *UserTest) DeleteByID(id int) error {
+	return nil
+}
+
+func (u *UserTest) Insert(user User) (int, error) {
+	return 2, nil
+}
+
+func (u *UserTest) ResetPassword(password string) error {
+	return nil
+}
+
+func (u *UserTest) PasswordMatches(plainText string) (bool, error) {
+	return true, nil
+}
+
+func (p *PlanTest) GetAll() ([]*Plan, error) {
+
+	var plans []*Plan
+
+	plan := Plan{
+		ID:         1,
+		PlanName:   "Plan Test",
+		PlanAmount: 1000,
+		CreatedAt:  time.Now(),
+		UpdatedAt:  time.Now(),
+	}
+
+	plans = append(plans, &plan)
+	return plans, nil
+}
+
+func (p *PlanTest) GetOne(id int) (*Plan, error) {
+	plan := Plan{
+		ID:         1,
+		PlanName:   "Plan Test",
+		PlanAmount: 1000,
+		CreatedAt:  time.Now(),
+		UpdatedAt:  time.Now(),
+	}
+
+	return &plan, nil
+}
+
+func (p *PlanTest) SubscribeUserToPlan(user User, plan Plan) error {
+	return nil
+}
+
+func (p *PlanTest) AmountForDisplay() string {
+	amount := float64(p.PlanAmount) / 100.0
+	return fmt.Sprintf("$%.2f", amount)
+}
